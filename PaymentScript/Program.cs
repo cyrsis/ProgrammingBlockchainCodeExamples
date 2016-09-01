7e972e6b8a83729d101322fb3b1d8763fd887d83
Victor <em3888@gmail.com>
2016-09-01 17:01:05 +0800

    Done Up to 1.2 ScriptPubKey
    Need to work with the relationship with Bitcoin Address and ScriptPubkey

--- PaymentScript/Program.cs
+++ PaymentScript/Program.cs
@@ -8,25 +8,31 @@ namespace PaymentScript
     {
         static void Main()
         {
+            //There is no know "bitcoin Address",all it goes to
+            //it actually all they care is ScriptPubKey
             var publicKeyHash = new KeyId("14836dbe7f38c5ac3d49e8d790af808a4ee9edcf");
 
             var testNetAddress = publicKeyHash.GetAddress(Network.TestNet);
             var mainNetAddress = publicKeyHash.GetAddress(Network.Main);
 
+            //** Here is what ScriptPubKey look like
             Console.WriteLine(mainNetAddress.ScriptPubKey); // OP_DUP OP_HASH160 14836dbe7f38c5ac3d49e8d790af808a4ee9edcf OP_EQUALVERIFY OP_CHECKSIG
             Console.WriteLine(testNetAddress.ScriptPubKey); // OP_DUP OP_HASH160 14836dbe7f38c5ac3d49e8d790af808a4ee9edcf OP_EQUALVERIFY OP_CHECKSIG
-
+            //They look the same, Are they really the same?
             
             var paymentScript = publicKeyHash.ScriptPubKey;
             var sameMainNetAddress = paymentScript.GetDestinationAddress(Network.Main);
             Console.WriteLine(mainNetAddress == sameMainNetAddress); // True
+            //generate bitcoin address from ScriptPubKey
 
-
+            //Retrieve the harsh from the ScriptPubKey and generate Bitcoin Address
+            //The ScriptPubKey does not contains the hashed public key permitted to spend the bitcoin
             var samePublicKeyHash = (KeyId)paymentScript.GetDestination();
             Console.WriteLine(publicKeyHash == samePublicKeyHash); // True
             var sameMainNetAddress2 = new BitcoinPubKeyAddress(samePublicKeyHash, Network.Main);
             Console.WriteLine(mainNetAddress == sameMainNetAddress2); // True
 
+            //From now on, we only exclusively user ScriptPubKey, Address is only a user interface concept
 
             Console.ReadLine();
         }
